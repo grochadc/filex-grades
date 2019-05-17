c7890ae1d1d2d901583f5b9545079a69e5ccc98f
Gonzalo Rocha <grochadc@gmail.com>
2019-05-17 14:08:55 -0500

    test/ Implement mockAxios

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -11,28 +11,48 @@ import {
   getByText,
   wait
 } from "react-testing-library";
-import axios from "axios";
+import mockAxios from "jest-mock-axios";
 
-afterEach(cleanup);
+afterEach(() => {
+  mockAxios.reset();
+  cleanup();
+});
 describe("App", () => {
   it("renders without crashing", () => {
     const { container } = render(<App />);
   });
   it("renders a list of students when clicked", async () => {
-    axios.get = jest.fn(() => Promise.resolve({ data: [grades] }));
     const { container } = render(<App />);
+    mockAxios.mockResponse({ data: [grades] });
     const link = getByTestId(container, "externos-link");
     fireEvent.click(link);
     await wait(() => getByTestId(container, "externo-item"));
     const students = getAllByTestId(container, "externo-item");
 
-    expect(axios.get).toHaveBeenCalledWith("http://localhost:3001/external");
+    expect(mockAxios.get).toHaveBeenCalledWith(
+      "http://localhost:3001/external"
+    );
     expect(students).toHaveLength(1);
     expect(students[0].textContent).toBe(grades.nombre);
   });
   it("renders a specific student's grades", async () => {
-    axios.get = jest.fn(() => Promise.resolve({ data: [grades] }));
     const { container } = render(<App />);
+    mockAxios.mockResponse({ data: [grades] });
+    const input = getByLabelText(container, "Code:");
+    input.value = grades.codigo;
+    fireEvent.change(input);
+    const button = getByTestId(container, "send-button");
+    fireEvent.click(button);
+
+    await wait(() => getByTestId(container, "codigo-name"));
+    const studentCode = getByText(container, "codigo-name");
+
+    expect(studentCode).toBe(grades.codigo);
+  });
+
+  it("renders a warning if a student is not found", async () => {
+    const { container } = render(<App />);
+    mockAxios.mockResponse({ data: { error: "404" } });
     const input = getByLabelText(container, "Code:");
     input.value = grades.codigo;
     fireEvent.change(input);
